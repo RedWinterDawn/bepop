eac795aafda77c35c703c50f76331aa3e08207eb
Brandon Pedersen <bpedersen@getjive.com>
2014-08-27 17:44:19 -0600

    [NS] Fix jumpy cli to not hard code nurse server

--- src/main/java/com/jive/v5/cli/jumpy/Main.java
+++ src/main/java/com/jive/v5/cli/jumpy/Main.java
@@ -2,25 +2,21 @@ package com.jive.v5.cli.jumpy;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import net.sourceforge.argparse4j.ArgumentParsers;
-import net.sourceforge.argparse4j.impl.Arguments;
-import net.sourceforge.argparse4j.inf.ArgumentParser;
-import net.sourceforge.argparse4j.inf.ArgumentParserException;
-import net.sourceforge.argparse4j.inf.Namespace;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.databind.type.SimpleType;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.jcraft.jsch.IdentityRepository;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.OpenSSHConfig;
@@ -31,26 +27,35 @@ import com.jcraft.jsch.agentproxy.USocketFactory;
 import com.jcraft.jsch.agentproxy.connector.SSHAgentConnector;
 import com.jcraft.jsch.agentproxy.usocket.NCUSocketFactory;
 import com.jive.jackson.ConstructorPropertiesAnnotationIntrospector;
+import com.jive.v5.cli.jumpy.jim.JimClient;
+import com.jive.v5.cli.jumpy.jim.JimInstance;
+import com.jive.v5.cli.jumpy.jim.JimNetwork;
+import com.jive.v5.commons.rest.client.RestClient;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 public class Main implements UserInfo
 {
 
   private final static String BASTION = "bastion";
 
-  public static void main(String[] arg)
+  public static void main(final String[] arg)
   {
 
-    Map<String, String> sites = new HashMap<>();
-
-    sites.put("pvu-1a", "10.117.255.100");
-    sites.put("dfw-1a", "10.118.255.100");
-
-    ArgumentParser argp = ArgumentParsers.newArgumentParser("jumpy")
-        .defaultHelp(true)
-        .usage("jumpy [args]")
-        .version(Main.class.getPackage().getImplementationVersion())
-        .description("Show jumpy service states.")
-        .epilog(String.format("Command version %s\n", Main.class.getPackage().getImplementationVersion()));
+    final ArgumentParser argp =
+        ArgumentParsers
+            .newArgumentParser("jumpy")
+            .defaultHelp(true)
+            .usage("jumpy [args]")
+            .version(Main.class.getPackage().getImplementationVersion())
+            .description("Show jumpy service states.")
+            .epilog(
+                String.format("Command version %s\n", Main.class.getPackage()
+                    .getImplementationVersion()));
 
     argp.addArgument("--version")
         .action(Arguments.version())
@@ -59,7 +64,6 @@ public class Main implements UserInfo
     argp.addArgument("-s", "--site")
         .dest("site")
         .setDefault("pvu-1a")
-        .choices(sites.keySet())
         .metavar("site")
         .help("Site to connect to jumpy");
 
@@ -68,39 +72,76 @@ public class Main implements UserInfo
         .metavar("ssh-user")
         .help("Bastion username");
 
-    Namespace ns;
+    final Namespace ns;
 
     try
     {
       ns = argp.parseArgs(arg);
     }
-    catch (ArgumentParserException e)
+    catch (final ArgumentParserException e)
     {
       argp.handleError(e);
       System.exit(1);
       return;
     }
 
-    try
+    final ObjectMapper mapper = new ObjectMapper()
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .enable(SerializationFeature.INDENT_OUTPUT)
+        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .registerModule(new JodaModule());
+    ConstructorPropertiesAnnotationIntrospector.install(mapper);
+
+    try (final CloseableHttpAsyncClient asyncClient = HttpAsyncClients.createDefault())
     {
+      asyncClient.start();
 
-      Path home = Paths.get(System.getProperty("user.home"), ".ssh");
+      final RestClient restClient = new RestClient(asyncClient, mapper);
+      final JimClient jimClient =
+          restClient.bind(
+              "http://jim.devops.jive.com:8080/",
+              JimClient.class,
+              (request) -> request.addHeader("Authorization",
+                  "Token " + System.getenv("JIM_APIKEY")));
 
-      JSch jsch = new JSch();
+      final List<JimInstance> instances = jimClient.listInstances("nurse").get();
+
+      final String site = ns.getString("site");
+
+      final JimInstance nurseServer = instances.stream()
+          .filter((instance) -> site.equals(instance.getSite()))
+          .findFirst()
+          .orElseThrow(() -> new IllegalArgumentException(
+              String.format("Unable to find nurse server to connect to in site [%s]", site)));
+
+      final JimNetwork devNetwork = nurseServer.getNetworks()
+          .stream()
+          .filter((network) -> network.getName().equals("dev")
+              || network.getName().equals("v4compat"))
+          .findFirst()
+          .orElseThrow(
+              () -> new IllegalStateException(String.format(
+                  "Unable to find network on nurse server to connect to. Networks: [%s]",
+                  nurseServer.getNetworks())));
+
+      final Path home = Paths.get(System.getProperty("user.home"), ".ssh");
+
+      final JSch jsch = new JSch();
 
       if (SSHAgentConnector.isConnectorAvailable())
       {
         // note: using the nc version to avoid an annoying window popping up every time!
-        USocketFactory usf = new NCUSocketFactory();
-        SSHAgentConnector con = new SSHAgentConnector(usf);
-        IdentityRepository irepo = new RemoteIdentityRepository(con);
+        final USocketFactory usf = new NCUSocketFactory();
+        final SSHAgentConnector con = new SSHAgentConnector(usf);
+        final IdentityRepository irepo = new RemoteIdentityRepository(con);
         jsch.setIdentityRepository(irepo);
       }
 
       jsch.setKnownHosts(home.resolve("known_hosts").toAbsolutePath().toString());
-      jsch.setConfigRepository(OpenSSHConfig.parseFile(home.resolve("config").toAbsolutePath().toString()));
+      jsch.setConfigRepository(OpenSSHConfig.parseFile(home.resolve("config").toAbsolutePath()
+          .toString()));
 
-      Session session = jsch.getSession(ns.getString("user"), BASTION, 22);
+      final Session session = jsch.getSession(ns.getString("user"), BASTION, 22);
 
       session.setConfig("PreferredAuthentications", "publickey");
       session.setConfig("StrictHostKeyChecking", "no");
@@ -111,30 +152,26 @@ public class Main implements UserInfo
 
       session.connect(5000);
 
-      int assinged_port = session.setPortForwardingL(0, sites.get(ns.getString("site")), 8080);
+      final int assignedPort =
+          session.setPortForwardingL(0, devNetwork.getAddress(), 8080);
 
       // now make a HTTP request to this port.
 
       try (CloseableHttpClient client = HttpClientBuilder.create().build())
       {
 
-        HttpGet req = new HttpGet(String.format("http://127.0.0.1:%d/jumpy", assinged_port));
+        final HttpGet req = new HttpGet(String.format("http://127.0.0.1:%d/jumpy", assignedPort));
 
         req.addHeader("Accept", "application/json");
 
         try (CloseableHttpResponse res = client.execute(req))
         {
 
-          ObjectMapper mapper = new ObjectMapper();
-          
-          mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-
-          ConstructorPropertiesAnnotationIntrospector.install(mapper);
-
-          List<Service> services = mapper.readValue(res.getEntity().getContent(),
+          final List<Service> services = mapper.readValue(res.getEntity().getContent(),
               CollectionType.construct(List.class, SimpleType.construct(Service.class)));
 
-          services.forEach((e) -> System.out.println(String.format("%35s %s", e.getName(), e.getProperties())));
+          services.forEach((e) -> System.out.println(String.format("%35s %s", e.getName(),
+              e.getProperties())));
 
         }
 
@@ -143,7 +180,7 @@ public class Main implements UserInfo
       session.disconnect();
 
     }
-    catch (Exception e)
+    catch (final Exception e)
     {
       System.out.println(e);
     }
@@ -163,25 +200,25 @@ public class Main implements UserInfo
   }
 
   @Override
-  public boolean promptPassphrase(String id)
+  public boolean promptPassphrase(final String id)
   {
     return true;
   }
 
   @Override
-  public boolean promptPassword(String arg0)
+  public boolean promptPassword(final String arg0)
   {
     return true;
   }
 
   @Override
-  public boolean promptYesNo(String arg0)
+  public boolean promptYesNo(final String arg0)
   {
     return true;
   }
 
   @Override
-  public void showMessage(String arg0)
+  public void showMessage(final String arg0)
   {
     System.err.println("MSG" + arg0);
   }
